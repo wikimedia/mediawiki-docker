1536a872d4f9eb62959cfe66d4109888b6337f1a
Krzysztof Szarlej <krzysztof.szarlej@airhelp.com>
2019-06-26 13:03:34 +0200

    Switch parsoid to alpine and upgrade to v0.10.0

--- parsoid/docker-entrypoint.sh
+++ parsoid/docker-entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-sed -i -e "s#/w/api.php#${MEDIAWIKI_API_ENDPOINT}#" -e "s#http://localhost#${MEDIAWIKI_URL}#g" /etc/mediawiki/parsoid/config.yaml
+
+# See https://github.com/wikimedia/parsoid/blob/master/config.example.yaml
+cat <<EOF > ${PARSOID_PATH}/config.yaml
+worker_heartbeat_timeout: 300000
+
+logging:
+    level: info
+
+services:
+  - module: lib/index.js
+    entrypoint: apiServiceWorker
+    conf:
+      mwApis:
+      - uri: '${MEDIAWIKI_URL}${MEDIAWIKI_API_ENDPOINT}'
+EOF
+
 exec $@
