e6be4c4782feb8596ab1953db3bc1b4e1ab55bd4
David Barratt <dbarratt@wikimedia.org>
2020-09-27 12:00:42 -0400

    Fix sort order on versions and update MediaWiki

--- update.sh
+++ update.sh
@@ -20,10 +20,9 @@ declare -A peclVersions=(
 )
 
 function mediawiki_version() {
-	git ls-remote --tags https://github.com/wikimedia/mediawiki.git \
+	git ls-remote --sort=version:refname --tags https://github.com/wikimedia/mediawiki.git \
 		| cut -d/ -f3 \
 		| tr -d '^{}' \
-		| sort -V \
 		| grep -E "^$1" \
 		| tail -1
 }
